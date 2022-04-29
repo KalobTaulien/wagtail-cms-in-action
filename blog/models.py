d125e928f21eaaca26ba4525e8c662ba3128f0f4
Kalob Taulien <kalob.taulien@gmail.com>
2022-04-29 12:40:57 -0600

    Adding Wagtail StreamFields

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,8 @@
+from wagtail.admin.panels import FieldPanel
 from wagtail.core.models import Page
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class BlogIndexPage(Page):
@@ -8,3 +12,16 @@ class BlogIndexPage(Page):
 
 class BlogPostPage(Page):
     parent_page_types = ['blog.BlogIndexPage']
+
+    body = StreamField([
+        ("title", blocks.CharBlock(
+            form_classname="full title",
+            template="blocks/title_block.html"
+        )),
+        ("richtext", blocks.RichTextBlock()),
+        ("image", ImageChooserBlock())
+    ], blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body')
+    ]
