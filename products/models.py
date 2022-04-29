2627cd2cf24f46ba1efe53a0aa8b35653e563be3
Kalob Taulien <kalob.taulien@gmail.com>
2022-04-29 13:20:53 -0600

    Orderables

--- products/models.py
+++ products/models.py
@@ -1,4 +1,8 @@
-from wagtail.core.models import Page
+from django.db import models
+from modelcluster.fields import ParentalKey
+
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.core.models import Page, Orderable
 
 
 class ProductIndexPage(Page):
@@ -9,3 +13,31 @@ class ProductIndexPage(Page):
 class ProductDetailPage(Page):
     parent_page_types = ['products.ProductIndexPage']
     subpage_types = []
+
+    content_panels = Page.content_panels + [
+        InlinePanel(
+            "product_images",
+            max_num=5,
+            min_num=1,
+            label="Product Images"
+        ),
+    ]
+
+
+class ProductImages(Orderable):
+    page = ParentalKey('products.ProductDetailPage', related_name="product_images")
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    alt_text = models.CharField(max_length=100, blank=False, default='')
+    short_description = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        FieldPanel("image"),
+        FieldPanel("alt_text"),
+        FieldPanel("short_description"),
+    ]
