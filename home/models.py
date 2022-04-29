c12d3ad7658c560d7d70d63189e0077088f5db61
Kalob Taulien <kalob.taulien@gmail.com>
2022-04-29 12:15:46 -0600

    Chapter 2: Setting up Wagtail

--- home/models.py
+++ home/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    subtitle = models.CharField(max_length=100, default='', blank=True)
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    linked_page = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('image'),
+        FieldPanel('linked_page'),
+    ]
