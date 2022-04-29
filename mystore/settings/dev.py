94cb93d3c1de5b0bbd5e18fdb9ae9c22e0cf3ae5
Kalob Taulien <kalob.taulien@gmail.com>
2022-04-29 11:59:27 -0600

    Initial Wagtail Setup

---
+++ mystore/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-j(68f)@#93jw$hpoe2)(ndzes8dlqo3^^89)2##l%x*+dc!%l%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
