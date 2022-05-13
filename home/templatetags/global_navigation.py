d4598bc5eaf5fed2876b9b517b7aa84dbfef6f96
Kalob Taulien <kalob.taulien@gmail.com>
2022-05-13 07:50:07 -0600

    Caching the menu system

--- home/templatetags/global_navigation.py
+++ home/templatetags/global_navigation.py
@@ -1,4 +1,6 @@
 from django import template
+from django.core.cache import cache
+
 from home.models import HomePage
 
 register = template.Library()
@@ -6,5 +8,11 @@ register = template.Library()
 
 @register.simple_tag
 def global_nav_items():
-    homepage = HomePage.objects.first()
-    return homepage.get_children().in_menu().live()
+    CACHE_NAME = "MENU_PAGES"
+    if cache.get(CACHE_NAME):
+        menu_pages = cache.get(CACHE_NAME)
+    else:
+        homepage = HomePage.objects.first()
+        menu_pages = homepage.get_children().in_menu().live()
+        cache.set(CACHE_NAME, menu_pages, 25200)
+    return menu_pages
